91797b4045b606de9afa86c3844826d70da8a2cc
TylerSmith4 <140591258+TylerSmith4@users.noreply.github.com>
2023-09-16 18:19:58 +1000

    Completed and uploaded Climate_Q2

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+
+df = pd.read_csv("climate.csv")
+
+years = list(df['Year'])
+co2 = list(df['CO2'])
+temp = list(df['Temperature'])
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
-
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
